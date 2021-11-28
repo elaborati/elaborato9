affe0b0e4397345c63c171c42f403a6a14e37231
luca-patrignani <luca0patrignani@gmail.com>
2021-11-28 15:36:55 +0100

    snake_create, kill, head, body

--- snake.c
+++ snake.c
@@ -1,25 +1,53 @@
 #include "snake.h" 
 #include <stdio.h>
-
+#include <stdlib.h>
 
 
 struct snake *snake_create(unsigned int rows, unsigned int cols) {
-	
-  return NULL;
+	struct snake* snake = malloc(sizeof(struct snake));
+	snake->rows = rows;
+	snake->cols = cols;
+	snake->length = 1;
+
+	snake->body = malloc(sizeof(struct body));
+	snake->body->next = NULL;
+	snake->body->prev = NULL;
+	struct position randPos = {rand() % rows, rand() % cols};
+	snake->body->pos = randPos;
+	return snake;
 } 
 
 void snake_kill(struct snake *s) {	
+	struct body* prev = s->body;
+	struct body* next;
+	while (prev != NULL) {
+		next = prev->next;
+	 	free(prev);
+		prev = next; 
+	}
+	free(s);
+	s = NULL;
 }
 
 struct position snake_head(struct snake *s) {
-	struct position p;
+	struct position p = s->body->pos;
 	return p;
 
 }
 
 struct position snake_body(struct snake *s, unsigned int i) {
-	struct position p;
-	return p;
+	if (i < s->length) {
+		unsigned n = 0;
+		struct body* node = s->body;
+		while (node != NULL) {
+			if(n == i)
+				return node->pos;
+			node = node->next;
+			++n; 
+		}
+	}
+	struct position unkPos = {0, 0};
+	return unkPos;
 }
 
 int snake_knotted(struct snake *s) {
